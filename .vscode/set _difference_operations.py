c0a40ae48811baab9b3633001a07b0916ecc036f
aswin <aswinpdas83@gmail.com>
2022-07-05 14:34:17 +0530

    # Hacker Rank Sets : Set .difference() Operation solution

---
+++ .vscode/set _difference_operations.py
@@ -0,0 +1,7 @@
+# Hacker Rank Sets : Set .difference() Operation solution
+if __name__ == '__main__':
+    n = int(input())
+    first_news_sub = set(map(int, input().split()))
+    n = int(input())
+    second_news_sub = set(map(int, input().split()))
+    print(len(first_news_sub.difference(second_news_sub)))
