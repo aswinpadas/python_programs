bafa72f6f11548100e0b4bd2c88c337709640e0d
aswin <aswinpdas83@gmail.com>
2022-07-05 16:53:09 +0530

    # Hacker Rank String => What is your name?

---
+++ whats_your_name.py
@@ -0,0 +1,9 @@
+# Hacker Rank String => What is your name?
+def print_full_name(first, last):
+    print("Hello " + first, last + "! You just delved into python.")
+
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
