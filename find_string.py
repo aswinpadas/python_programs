89a5fc1b92da60998fce3edeaed712fdd0fc93b8
aswin <aswinpdas83@gmail.com>
2022-07-06 15:30:41 +0530

    another method of find string

--- find_string.py
+++ find_string.py
@@ -1,15 +1,34 @@
 #Hacker Rank String => Find a string
+#method 1
+# def count_substring(string, query, count):
+#     idx = string.find(query)
+#     return count_substring(string[idx + 1:], query, count +
+#                            1) if idx != -1 else count
+
+# if __name__ == '__main__':
+#     string = input().strip()
+#     sub_string = input().strip()
+
+#     count = count_substring(string, sub_string, 0)
+#     print(count)
+#     print(
+#         sum([
+#             1 for i in range(len(string) - len(sub_string) + 1)
+#             if string[i:i + len(sub_string)] == sub_string
+#         ]))
 
 
-def count_substring(string, query, count):
-    idx = string.find(query)
-    return count_substring(string[idx + 1:], query, count +
-                           1) if idx != -1 else count
+#metheod 2
+def count_substring(string, sub_string):
+    return sum([
+        1 for i in range(len(string) - len(sub_string) + 1)
+        if string[i:i + len(sub_string)] == sub_string
+    ])
 
 
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
 
-    count = count_substring(string, sub_string, 0)
+    count = count_substring(string, sub_string)
     print(count)
