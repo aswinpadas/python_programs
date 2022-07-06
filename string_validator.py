b7fefe33d6d41509159adeb7f43439fe01241c6a
aswin <aswinpdas83@gmail.com>
2022-07-06 17:04:35 +0530

    add another method : for future references

--- string_validator.py
+++ string_validator.py
@@ -8,3 +8,10 @@ if __name__ == '__main__':
     print(any([item.isdigit() for item in s]))
     print(any([item.islower() for item in s]))
     print(any([item.isupper() for item in s]))
+    print('\n\n**************************\n\n')
+
+    # another method : for future references
+    for method in [
+            str.isalnum, str.isalpha, str.isdigit, str.islower, str.isupper
+    ]:
+        print(any(method(c) for c in s))
