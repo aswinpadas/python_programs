33800f83db0a74c57b79b6332a90df2011f5c1e6
aswin <aswinpdas83@gmail.com>
2022-07-08 18:00:01 +0530

    workout programs and other

---
+++ test.py
@@ -0,0 +1,57 @@
+# s = 'chcshdcsdcvcgvagc'
+# print("".join(sorted(list(s))))
+
+# print(*["".join(sorted(list(s)))[i:i + 4] for i in range(0, len(s), 4)], )
+
+# import decimal
+
+# n = 17
+# print(decimal.Decimal(17))
+
+# number = 17
+# print([
+#     x + '\t' + (oct(x)[2:] + '\t' + hex(x).upper()[2:] + '\t' + bin(x)[2:])
+#     for x in range(1, 18)
+# ])
+
+# print(*[
+#     x + '\t' + (oct(x)[2:] + '\t' + hex(x).upper()[2:] + '\t' + bin(x)[2:])
+#     for x in range(1, 18)
+# ],
+#       sep='\n')
+# number = 17
+# print('\n'.join(" ".join([str(x).rjust(len(format(number, 'b'))),format(x, 'o').rjust(len(format(number, 'b'))),format(x, 'X').rjust(len(format(number, 'b'))),format(x, 'b').rjust(len(format(number, 'b')))]) for x in range(1, number + 1)))
+
+# def print_formatted(number):
+#     # your code goes here
+#     return '\n'.join(
+#         "\t".join([str(x) + format(x, 'o') + format(x, 'X') + format(x, 'b')])
+#         for x in range(1, number + 1))
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     print_formatted(n)
+
+# s = 'christy saji'
+# print(''.join(str(x[0].upper() + x[1:] + ' ') for x in [j for j in s.split(" ")]))
+
+#!/bin/python3
+
+# import math
+# import os
+# import random
+# import re
+# import sys
+
+# # Complete the solve function below.
+# s = '1 w 2 r 3g'
+# #print(" ".join([str(x).capitalize() for x in s.split(" ")]))
+# print(' '.join(map(str.capitalize, s.split(' '))))
+# from itertools import product
+
+# A = [1, 2]
+# B = [3, 4]
+
+# print(" ".join((str(x)) for x in (product(A, B))))
+
+print()
