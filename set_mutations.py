9d4b8820d440907c634ffc97ce8a8245767f84e5
aswin <aswinpdas83@gmail.com>
2022-07-19 15:11:56 +0530

    lint

--- set_mutations.py
+++ set_mutations.py
@@ -1,4 +1,5 @@
 # Hacker Rank Set => Mutations solution
+
 _, a = (int(input()), set(map(int, input().split())))
 for x in range(0, int(input())):
     operation_name, b = (input().split()[0], set(map(int, input().split())))
