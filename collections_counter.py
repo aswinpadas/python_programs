5a531a3c67ef22b742f24ba78407a414e4175081
aswin <aswinpdas83@gmail.com>
2022-07-19 12:15:59 +0530

    Hacker Rank Collection => Collecion Counter soutions

--- collections_counter.py
+++ collections_counter.py
@@ -1,10 +1,14 @@
+# Hacker Rank Collection => Collecion Counter soutions
+
 from collections import Counter
 from itertools import count
 
 n = int(input())
-available_sizes = list(map(int, input().split()))[:n]
-print(available_sizes)
-number_coustomers = int(input())
-# purchase = [[map(int,
-#                  input().split())[:2][x]] for x in range(0, number_coustomers)]
-# print(purchase)
+available_sizes = Counter(list(map(int, input().split()))[:n])
+purchase_amt = 0
+for x in range(0, int(input())):
+    size, rate = input().split()
+    if available_sizes[int(size)] != None and available_sizes[int(size)] != 0:
+        available_sizes[int(size)] -= 1
+        purchase_amt += int(rate)
+print(purchase_amt)
