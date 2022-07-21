fa83a9fd5f9801d20e42e53788f2f61c30734718
aswin <aswinpdas83@gmail.com>
2022-07-21 12:41:51 +0530

    Hacker Rank String => Merge the Tools!

---
+++ string_merge_tools.py
@@ -0,0 +1,25 @@
+# Hacker Rank String => Merge the Tools!
+
+from collections import OrderedDict
+from textwrap import fill
+# single statement
+print(*[
+    "".join(OrderedDict.fromkeys(i))
+    for i in fill(input(), int(input())).split("\n")
+],
+      sep=("\n"))
+
+#-------------------------------------------------------------------------------------
+
+#  just Wrapped in a function  :  Hacker rank default code
+
+# from collections import OrderedDict
+# from textwrap import fill
+
+# def merge_the_tools(string, k):
+#     # your code goes here
+#     print(*["".join(OrderedDict.fromkeys(i)) for i in fill(string, k).split("\n")], sep=("\n"))
+
+# if __name__ == '__main__':
+#     string, k = input(), int(input())
+#     merge_the_tools(string, k)
