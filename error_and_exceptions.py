5c130dcb8353bafa449d51a94748e7a2c705a81d
aswin <aswinpdas83@gmail.com>
2022-07-26 13:12:41 +0530

    #Haker Rank Exception and Errors => Exceptions soulution

---
+++ error_and_exceptions.py
@@ -0,0 +1,7 @@
+# Haker Rank Exception and Errors => Exceptions soulution
+for _ in range(int(input())):
+    try:
+        a, b = input().split()
+        print(int(a) // int(b))
+    except Exception as e:
+        print("Error Code:", e)
