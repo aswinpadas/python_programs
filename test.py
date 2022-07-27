286f07d38c6575664c667371d988a50718175912
aswin <aswinpdas83@gmail.com>
2022-07-27 16:12:51 +0530

    hakeer rank 30 days Day 2: Operators

--- test.py
+++ test.py
@@ -132,6 +132,22 @@
 # test(y)
 # print(y)
 # print("hello World\n" + input())
-print(int(input()) + 4)
-print(float(input()) + 4)
-print("HackerRank", input())
+# print(int(input()) + 4)
+# print(float(input()) + 4)
+# print("HackerRank", input())
+
+
+def solve(meal_cost, tip_percent, tax_percent):
+    print(
+        round(((tip_percent / 100) * meal_cost) +
+              ((tax_percent / 100) * meal_cost) + meal_cost))
+
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
