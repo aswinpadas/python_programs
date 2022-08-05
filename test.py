795932a7fa5d5047e1308e484c968c56a73ad198
aswin <aswinpdas83@gmail.com>
2022-08-05 14:18:47 +0530

    30 Days of CodeDay 13: Abstract Classes

--- test.py
+++ test.py
@@ -255,13 +255,47 @@
 #             sum = 0
 #     print((tarr))
 
-grade = {
-    range(90, 101): "O",
-    range(80, 90): "E",
-    range(70, 80): "A",
-    range(55, 70): "P",
-    range(40, 55): "D",
-    range(0, 40): "T",
-}
+# Dict range or rangfe oin dict as key
+# grade = {
+#     range(90, 101): "O",
+#     range(80, 90): "E",
+#     range(70, 80): "A",
+#     range(55, 70): "P",
+#     range(40, 55): "D",
+#     range(0, 40): "T",
+# }
 
-print(*[y for x, y in grade.items() if 55 in x])
+# print(*[y for x, y in grade.items() if 55 in x])
+
+from abc import ABCMeta, abstractmethod
+
+
+class Book(object, metaclass=ABCMeta):
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display():
+        pass
+
+
+class MyBook(Book):
+
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def display(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Price:", self.price)
+
+
+title = input()
+author = input()
+price = int(input())
+new_novel = MyBook(title, author, price)
+new_novel.display()
