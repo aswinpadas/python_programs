c8ef5533246b854f9ee4a0560cbda9eabb4b1656
aswin <aswinpdas83@gmail.com>
2022-08-07 21:38:14 +0530

    Day 0: Mean, Median, and Mode Solution using Numpy

--- mean_median_mode.py
+++ mean_median_mode.py
@@ -8,3 +8,13 @@ print("{:.1f}".format(sum(values) / len(values)))
 print((values[int(length / 2) - 1] + values[int(length / 2)]) / 2)
 values = collections.Counter(values)
 print(values.most_common()[0][0])
+""" Solution using Numpy"""
+
+import numpy as np
+from scipy import stats
+
+size = int(input())
+numbers = list(map(int, input().split()))
+print(np.mean(numbers))
+print(np.median(numbers))
+print(int(stats.mode(numbers)[0]))
