f5f1442501503047b6dfc20ce766837e254becf9
aswin <aswinpdas83@gmail.com>
2022-08-09 17:25:00 +0530

    Hacker Rank itertools => Maximize it! Solutions

--- itertools_maximize_it.py
+++ itertools_maximize_it.py
@@ -1,11 +1,19 @@
-# Hacker Rank itertools => Maximize it! Solutions
+"""Hacker Rank itertools => Maximize it! Solutions"""
 
+from itertools import product
+
+K, M = map(int, input().split())
+N = (list(map(int, input().split()))[1:] for _ in range(K))
+results = map(lambda x: sum(i**2 for i in x) % M, product(*N))
+print(max(results))
+
+# other test solutions not working
 #not completed
 
-km = list(map(int, input().split()))
-sum = 0
-s = (list(map(int, input().split()))[1:] for _ in range(km[0]))
-print(*[[x**2 % km[1] for x in i] for i in s])
+# km = list(map(int, input().split()))
+# sum = 0
+# s = (list(map(int, input().split()))[1:] for _ in range(km[0]))
+# print(*[[x**2 % km[1] for x in i] for i in s])
 
 # for _ in range(km[0]):
 #     max_of_list = max(list(map(int, input().split())))
@@ -14,12 +22,3 @@ print(*[[x**2 % km[1] for x in i] for i in s])
 #     sum += sq_max_of_list
 # #     print('sum = ', sum)
 # print(max(sum % km[1]))
-
-from itertools import product
-
-k, m = map(int, input().split())
-n = (list(map(int, input().split()))[1:] for _ in range(k))
-# print(list(product(n)))
-results = map(lambda x: sum(i**2 for i in x) % m, product(*n))
-print(list(results))
-print(max(results))
