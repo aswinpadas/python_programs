834c02d3c72f760165c474f60735b9bbd1f99101
aswin <aswinpdas83@gmail.com>
2022-08-27 09:43:37 +0530

    Solution

---
+++ interview_machine_test.py
@@ -0,0 +1,21 @@
+def get_indices(nums, target):
+    length = len(nums)
+    for i in range(0, length):
+        for j in range(i + 1, length):
+            if nums[i] + nums[j] == target:
+                return ([i, j])
+
+
+# testcases
+
+print(get_indices([1, 2, 3], 3))
+print(get_indices([3, 2, 4], 6))
+print(get_indices([2, 7, 11, 15], 9))
+print(
+    get_indices(
+        [12, 15, 18, 21, 24, -27, 42, 45, 48, 57, 60, 72, -75, 96, 99, -102],
+        -60))
+print(
+    get_indices(
+        [12, 15, 18, 21, 24, -27, 42, 45, 48, 57, 60, 72, -75, 96, 99, -102],
+        -50))
