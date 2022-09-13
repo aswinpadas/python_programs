bd196a3afa6d05d4e1f65f608adf26e91cb45065
aswin <aswinpdas83@gmail.com>
2022-09-13 13:14:45 +0530

    linting issue corrcted.

--- target_get_indices.py
+++ target_get_indices.py
@@ -1,8 +1,10 @@
-""" For fuure reference 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. 
-You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return
-the answer in any order.
-algorithm that is less than O(n²) time complexity
+# pylint: disable=missing-function-docstring
+""" For fuure reference.
+Given an array of integers nums and an integer target, return indices of the two numbers such
+that they add up to target. You may assume that each input would have exactly one solution, and
+you may not use the same element twice. You can return the answer in any order.
+
+Algorithm that is less than O(n²) time complexity.
 """
 
 # Solution: complexity O(n).
