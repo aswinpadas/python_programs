36c0b13f6ccceb676bebac0f33efd501ab0e1962
aswin <aswinpdas83@gmail.com>
2022-09-13 13:09:03 +0530

    complexity mention.

--- target_get_indices.py
+++ target_get_indices.py
@@ -2,7 +2,10 @@
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. 
 You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return
 the answer in any order.
+algorithm that is less than O(n²) time complexity
 """
+
+# Solution: complexity O(n).
 
 
 def get_indices(nums, target):
